e213fffb490715624b4727b55467bf02b4c4f9d3
math-pixel <59537610+math-pixel@users.noreply.github.com>
2025-10-30 16:30:37 +0100

    update model DB

---
+++ src/Entity/Group.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GroupRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GroupRepository::class)]
+#[ORM\Table(name: '`group`')]
+class Group
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::GUID)]
+    private ?string $token = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $url_image1 = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $url_image2 = null;
+
+    /**
+     * @var Collection<int, UserGroup>
+     */
+    #[ORM\OneToMany(targetEntity: UserGroup::class, mappedBy: 'group_id')]
+    private Collection $userGroups;
+
+    /**
+     * @var Collection<int, Photo>
+     */
+    #[ORM\OneToMany(targetEntity: Photo::class, mappedBy: 'group_id')]
+    private Collection $photos;
+
+    public function __construct()
+    {
+        $this->userGroups = new ArrayCollection();
+        $this->photos = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): static
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUrlImage1(): ?string
+    {
+        return $this->url_image1;
+    }
+
+    public function setUrlImage1(string $url_image1): static
+    {
+        $this->url_image1 = $url_image1;
+
+        return $this;
+    }
+
+    public function getUrlImage2(): ?string
+    {
+        return $this->url_image2;
+    }
+
+    public function setUrlImage2(?string $url_image2): static
+    {
+        $this->url_image2 = $url_image2;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, UserGroup>
+     */
+    public function getUserGroups(): Collection
+    {
+        return $this->userGroups;
+    }
+
+    public function addUserGroup(UserGroup $userGroup): static
+    {
+        if (!$this->userGroups->contains($userGroup)) {
+            $this->userGroups->add($userGroup);
+            $userGroup->setGroupId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserGroup(UserGroup $userGroup): static
+    {
+        if ($this->userGroups->removeElement($userGroup)) {
+            // set the owning side to null (unless already changed)
+            if ($userGroup->getGroupId() === $this) {
+                $userGroup->setGroupId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Photo>
+     */
+    public function getPhotos(): Collection
+    {
+        return $this->photos;
+    }
+
+    public function addPhoto(Photo $photo): static
+    {
+        if (!$this->photos->contains($photo)) {
+            $this->photos->add($photo);
+            $photo->setGroupId($this);
+        }
+
+        return $this;
+    }
+
+    public function removePhoto(Photo $photo): static
+    {
+        if ($this->photos->removeElement($photo)) {
+            // set the owning side to null (unless already changed)
+            if ($photo->getGroupId() === $this) {
+                $photo->setGroupId(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Photo.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhotoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhotoRepository::class)]
+class Photo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $media = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $commentary = null;
+
+    #[ORM\Column]
+    private ?bool $is_allowed = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\ManyToOne(inversedBy: 'photos')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Group $group_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMedia(): ?string
+    {
+        return $this->media;
+    }
+
+    public function setMedia(string $media): static
+    {
+        $this->media = $media;
+
+        return $this;
+    }
+
+    public function getCommentary(): ?string
+    {
+        return $this->commentary;
+    }
+
+    public function setCommentary(?string $commentary): static
+    {
+        $this->commentary = $commentary;
+
+        return $this;
+    }
+
+    public function isAllowed(): ?bool
+    {
+        return $this->is_allowed;
+    }
+
+    public function setIsAllowed(bool $is_allowed): static
+    {
+        $this->is_allowed = $is_allowed;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getGroupId(): ?Group
+    {
+        return $this->group_id;
+    }
+
+    public function setGroupId(?Group $group_id): static
+    {
+        $this->group_id = $group_id;
+
+        return $this;
+    }
+}
