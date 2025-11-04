753748a3511643c0fb9afd25bb4bc6009450975f
math-pixel <59537610+math-pixel@users.noreply.github.com>
2025-11-04 15:22:07 +0100

    feat(group-bar) Create a group bar comopnent and services for get groups

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Service\GroupService;
+use App\Entity\Group;
+use App\Entity\UserGroup;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
+
+    public function __construct(
+        private GroupService $groupService
+    ) {}
+
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $user = $this->getUser();
+        $groups = $this->groupService->getGroupsOfUser($user);
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'groups' => $groups
         ]);
     }
 }
