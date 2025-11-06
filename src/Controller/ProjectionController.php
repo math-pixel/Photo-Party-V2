05e3bb065c81b6528a69b23bf10a56c02aa53983
math-pixel <59537610+math-pixel@users.noreply.github.com>
2025-11-06 19:27:10 +0100

    update(projector) style

--- src/Controller/ProjectionController.php
+++ src/Controller/ProjectionController.php
@@ -3,15 +3,29 @@
 // src/Controller/ProjectionController.php
 namespace App\Controller;
 
+use App\Entity\Group;
+use App\Entity\Photo;
+use App\Entity\UserGroup;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProjectionController extends AbstractController
 {
-    #[Route('/projection', name: 'app_projection', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/projection/{id}', name: 'app_projection', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function index(Group $group, EntityManagerInterface $em): Response
     {
-        return $this->render('projection/index.html.twig');
+
+        $groupId = $group->getId();
+
+        $photos = $em->getRepository(Photo::class)->find([
+            'id' => $groupId,
+        ]);
+
+        return $this->render('projection/index.html.twig', [
+            'groupParameter' => $group,
+            'photos' => $photos,
+        ]);
     }
 }
